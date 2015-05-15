3c1decb2e542290f96fda3c4c765e788c28e1c79
Bojie Li <bojieli@gmail.com>
2015-05-15 20:41:26 +0800

    add image.rb: download images and detect faces

--- examples/image.rb
+++ examples/image.rb
@@ -2,20 +2,54 @@
 $:.unshift File.dirname(__FILE__) + '/../lib'
 require 'rubygems'
 require 'google-search'
-require 'rext/all'
+require 'open-uri'
+require 'opencv'
+include OpenCV
 
-HTML = File.dirname(__FILE__) + '/images.html'
+if ARGV.length < 2
+  puts "Usage: ruby #{__FILE__} infile outfile"
+  exit
+end
+infile, outfile = ARGV
 
-File.open(HTML, 'w+') do |file|
-  Google::Search::Image.new(:query => 'Cookies', :image_size => :icon).each do |image|
-    file.write %(<img src="#{image.uri}">)
+tmpdir = './download'
+data = '../examples/data/haarcascade_frontalface_alt.xml'
+detector = CvHaarClassifierCascade::load(data)
+outfile = File.open(outfile, 'w')
+
+File.open(infile).each do |line|
+  line = line.strip
+  Google::Search::Image.new(:query => line + ' site:ustc.edu.cn').each do |image|
+    print "GET ", line, "\t", image.uri, "\t"
+    extname = File.extname(image.uri).downcase
+    if ['.gif'].include? extname
+      print "extension " + extname + " not recognized\n"
+      next
+    end
+    savename = tmpdir + '/' + line + extname
+    begin
+      File.open(savename, 'wb') do |file|
+        file << open(image.uri).read 
+      end
+    rescue
+      print "Download failed\n"
+      next
+    end
+    begin
+      cv_image = CvMat.load(savename)
+      num_people = detector.detect_objects(cv_image).length
+      if num_people == 1
+        outfile.write line + "\t" + image.uri + "\n"
+        outfile.flush
+        print "OK\n"
+        break
+      else
+        print "recognized " + num_people + " people\n"
+        next
+      end
+    rescue
+      print "OpenCV read failed\n"
+      next
+    end
   end
 end
-
-begin
-  `open -a Safari #{HTML}`
-rescue
-  puts "Not on OSX? wtf? open #{HTML} in your browser to view the images"
-end
-
-
